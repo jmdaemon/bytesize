ef96656b9fe0133bb939db7a1e0ee5c8591ac0d5
Joseph Diza <josephm.diza@gmail.com>
2022-04-03 18:22:36 -0700

    Add bytesize functions to header

--- include/bytesize.h
+++ include/bytesize.h
@@ -2,6 +2,13 @@
 #ifndef BYTESIZE_H
 #define BYTESIZE_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <pcre.h>
+#include <math.h>
+
 //#include "cli.h"
 
 #define SIZE 4
@@ -19,5 +26,13 @@ static const char *BYTE[SIZE] = {
   "GiB",
   "TiB"
 };
+
+long int calc_factor(const char *unit, int size, const char *BYTE_FORMAT[], int scale);
+
+bool found_in(const char *elem, const char *array[], int array_size);
+
+long int get_factor(const char *unit);
+
+const char *match(char *input, const char *regex);
 
 #endif // BYTESIZE_H
