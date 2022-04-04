2cbde003511b0a494b7f1505fe0705e06a17fd1c
Joseph Diza <josephm.diza@gmail.com>
2022-04-03 18:50:32 -0700

    Inline verbose flag

--- src/bytesize.c
+++ src/bytesize.c
@@ -104,13 +104,12 @@ int main (int argc, char **argv) {
      be reflected in arguments. */
   argp_parse (&argp, argc, argv, 0, 0, &arguments);
 
-  int verbose = arguments.verbose;
-
   const char *units_from = get_unit(arguments.args[0]);
   const char *units_to   = get_unit(arguments.args[1]);
-  const double conversion = convert_units(arguments.args[0], units_from, units_to, verbose);
+  const double conversion = convert_units(arguments.args[0], units_from, units_to, arguments.verbose);
 
   display_units(conversion, units_to, arguments.display_units);
+
   pcre_free_substring(units_from);
   pcre_free_substring(units_to);
   return 0;
