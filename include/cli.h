b9b3868680ba83b15b3ffeedb76fe3eb65fbfa2a
Joseph Diza <josephm.diza@gmail.com>
2022-04-11 13:25:46 -0700

    Silence compiler warnings about missing initializer fields

--- include/cli.h
+++ include/cli.h
@@ -19,10 +19,11 @@ static char doc[] = "Bytesize - Convert between byte representations";
 /* A description of the arguments we accept. */
 static char args_doc[] = "[from] [to]";
 
+/* http://www.gnu.org/software/libc/manual/html_node/Argp-Option-Vectors.html */
 /* The options we understand. */
 static struct argp_option options[] = {
-  {"verbose",  'v', 0,      0,  "Produce verbose output" },
-  {"units",  'u', 0,      0,  "Do not display byte units" },
+  {"verbose",  'v', 0,      0,  "Produce verbose output", 0},
+  {"units",  'u', 0,      0,  "Do not display byte units", 0},
   { 0 }
 };
 
@@ -36,8 +37,10 @@ struct arguments {
 /* Parse a single option. */
 error_t parse_opt (int key, char *arg, struct argp_state *state);
 
+/* http://www.gnu.org/software/libc/manual/html_node/Argp-Parsers.html */
+/* http://www.gnu.org/software/libc/manual/html_node/Argp-Help-Filtering.html */
 /* Our argp parser. */
-static struct argp argp = { options, parse_opt, args_doc, doc };
+static struct argp argp = { options, parse_opt, args_doc, doc, 0, 0, 0};
 
 /* Set default values for arguments */
 struct arguments set_default_args();
