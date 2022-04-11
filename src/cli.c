901742ac95980f69b63ee94db478cb22b1309451
Joseph Diza <josephm.diza@gmail.com>
2022-04-10 18:24:25 -0700

    Create parse_opt function in cli

--- src/cli.c
+++ src/cli.c
@@ -8,3 +8,35 @@ struct arguments set_default_args() {
   arguments.display_units = true;
   return arguments;
 }
+
+error_t parse_opt (int key, char *arg, struct argp_state *state) {
+  /* Get the input argument from argp_parse, which we
+     know is a pointer to our arguments structure. */
+  struct arguments *arguments = state->input;
+  switch (key) {
+    case 'v':
+      arguments->verbose = 1;
+      break;
+
+    case 'u':
+      arguments->display_units = false;
+      break;
+
+    case ARGP_KEY_ARG:
+      if (state->arg_num >= 2)
+        /* Too many arguments. */
+        argp_usage (state);
+      arguments->args[state->arg_num] = arg;
+      break;
+
+    case ARGP_KEY_END:
+      if (state->arg_num < 2)
+        /* Not enough arguments. */
+        argp_usage (state);
+      break;
+
+    default:
+      return ARGP_ERR_UNKNOWN;
+    }
+  return 0;
+}
