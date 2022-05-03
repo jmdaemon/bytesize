8019d3a3e0ae24951b592297f0cc69b8c3e8cfe3
Joseph Diza <josephm.diza@gmail.com>
2022-05-03 00:26:49 -0700

    Add test for calc_factor si_byte

--- test/test_bytesize.c
+++ test/test_bytesize.c
@@ -13,6 +13,22 @@ void tearDown(void) {
 }
 
 void calc_factor_si_should_return_correct_bytescale() {
+    // The scale is with the respect of the unit to the scale of one byte measurement
+    const char* unit = "KB";
+    const unsigned int scale = 1000;
+    const long int expected = 1000;
+    const long int result = calc_factor(unit, SIZE, SI_BYTE, scale);
+    // 1 KB == 1000 Bytes
+    TEST_ASSERT_EQUAL_INT(expected, result);
+}
+
+
+void calc_factor_si_byte_should_return_correct_bytescale() {
+    const char* unit = "B";
+    const unsigned int scale = 1;
+    const long int expected = 1;
+    const long int result = calc_factor(unit, SIZE, SI_BYTE, scale);
+    TEST_ASSERT_EQUAL_INT(expected, result);
 }
 
 void calc_factor_binary_should_return_correct_bytescale() {
@@ -34,6 +50,7 @@ void conversion_binary_to_si_should_return_correct_bytescale() {
 int main(void) {
     UNITY_BEGIN();
     RUN_TEST(calc_factor_si_should_return_correct_bytescale);
+    RUN_TEST(calc_factor_si_byte_should_return_correct_bytescale);
     RUN_TEST(calc_factor_binary_should_return_correct_bytescale);
     RUN_TEST(conversion_binary_to_si_should_return_correct_bytescale);
     return UNITY_END();
