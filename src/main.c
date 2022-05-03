5a108ef3f9a479ff74ed90ea0624a3dbcbb063e3
Joseph Diza <josephm.diza@gmail.com>
2022-05-03 01:23:31 -0700

    Use verbose parameter to show logging information

--- src/main.c
+++ src/main.c
@@ -10,20 +10,21 @@ int main (int argc, char **argv) {
 
   int verbose = arguments.verbose;
 
-  if (verbose == 1) {
-    log_info("Arguments: ");
-    log_info("Args[0]: %s\n", arguments.args[0]);
-    log_info("Args[1]: %s\n", arguments.args[1]);
-    log_info("Verbose: %s\n", arguments.verbose);
-    log_info("Display with units: %s\n", arguments.display_units);
-  }
+  // Set log level
+  if (verbose == 1) 
+    log_set_level(LOG_INFO);
+
+  log_info("Arguments: ");
+  log_info("Args[0]: %s\n", arguments.args[0]);
+  log_info("Args[1]: %s\n", arguments.args[1]);
+  log_info("Verbose: %s\n", arguments.verbose);
+  log_info("Display with units: %s\n", arguments.display_units);
 
   const char *units_from = get_unit(arguments.args[0]);
   const char *units_to   = get_unit(arguments.args[1]);
-  if (verbose == 1) {
-    log_info("Units From: %s\n", units_from);
-    log_info("Units To: %s\n", units_to);
-  }
+  log_info("Units From: %s\n", units_from);
+  log_info("Units To: %s\n", units_to);
+
   const double conversion = convert_units(arguments.args[0], units_from, units_to, arguments.verbose);
 
   display_units(conversion, units_to, arguments.display_units);
