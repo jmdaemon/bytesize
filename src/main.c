2a6af8c2c33a1fe8877800030e9c7a8c319c6bf1
Joseph Diza <josephm.diza@gmail.com>
2022-07-31 18:58:45 -0700

    Fix memleak on automatic size mode

--- src/main.c
+++ src/main.c
@@ -48,6 +48,8 @@ int main (int argc, char **argv) {
   if (smatch(output, "Auto")) 
     to = (is_byte(units_from)) ?
       auto_size(amt, scale, true) : auto_size(amt, get_factor(units_from), false);
+      /*auto_size(amt, get_factor(units_from), true) : auto_size(amt, get_factor(units_from), false);*/
+      /*auto_size(amt, scale, true) : auto_size(amt, get_factor(units_from), false);*/
   else 
     to = convert_units(input, units_from, units_to);
 
@@ -66,7 +68,11 @@ int main (int argc, char **argv) {
   if (!smatch(output, "Auto"))
     pcre_free_substring(units_to);
 
-  mpfr_clears(to.amt, amt, conversion, NULL); /* Deallocate the to.amt mpfr value here */
+  /*mpfr_clears(to.amt, amt, conversion, NULL); [> Deallocate the to.amt mpfr value here <]*/
+  if (!smatch(output, "Auto"))
+    mpfr_clear(amt);
+  mpfr_clear(to.amt);
+  mpfr_clear(conversion); /* Deallocate the to.amt mpfr value here */
   mpfr_free_cache2(MPFR_FREE_GLOBAL_CACHE);
   return 0;
 }
