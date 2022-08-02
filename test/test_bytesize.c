7bcc7fd846cade12cbf4c5b1a329e9705849b035
Joseph Diza <josephm.diza@gmail.com>
2022-08-02 14:44:33 -0700

    Update bytesize tests
    Note that the tests are not yet able to be executed with ctest just yet.

--- test/test_bytesize.c
+++ test/test_bytesize.c
@@ -1,12 +1,13 @@
 #include "bytesize.h"
 #include "unity.h"
 
-void setUp(void) {
-    /* set stuff up here */
-}
+Byte to;
+
+void setUp(void) { }
 
 void tearDown(void) {
-    /* clean stuff up here */
+    mpfr_clears(to.amt, NULL);
+    mpfr_free_cache2(MPFR_FREE_GLOBAL_CACHE);
 }
 
 void num_regex_should_return_num() {
@@ -41,7 +42,7 @@ void calc_factor_si_should_return_correct_bytescale() {
     // The scale is with the respect of the unit to the scale of one byte measurement
     const char* unit = "KB";
     const long int expected = 1000;
-    const long int result = calc_factor(unit, SIZE, SI_BYTE, SI_SCALE);
+    const long int result = calc_factor(unit, SIZE, SI);
     // 1 KB == 1000 Bytes
     TEST_ASSERT_EQUAL_INT(expected, result);
 }
@@ -50,8 +51,7 @@ void calc_factor_binary_should_return_correct_bytescale() {
     // The scale is with the respect of the unit to the scale of one byte measurement
     const char* unit = "KiB";
     const long int expected = 1024;
-    const long int result = calc_factor(unit, SIZE, BYTE, BINARY_SCALE);
-    // 1 KiB == 1024 Bytes
+    const long int result = calc_factor(unit, SIZE, BINARY);
     TEST_ASSERT_EQUAL_INT(expected, result);
 }
 
@@ -59,8 +59,8 @@ void calc_factor_byte_should_return_correct_bytescale() {
     // 1 B == 1 B
     const char* unit = "B";
     const long int expected = 1;
-    const long int binary_byte = calc_factor(unit, SIZE, BYTE, BINARY_SCALE);
-    const long int si_byte = calc_factor(unit, SIZE, SI_BYTE, SI_SCALE);
+    const long int binary_byte = calc_factor(unit, SIZE, BINARY);
+    const long int si_byte = calc_factor(unit, SIZE, SI);
     TEST_ASSERT_EQUAL_INT(expected, binary_byte);
     TEST_ASSERT_EQUAL_INT(expected, si_byte);
 }
@@ -70,8 +70,12 @@ void convert_units_binary_to_si_should_return_correct_conversion() {
     const char* from = "5MiB";
     const char* units_from = "MiB";
     const char* units_to = "MB";
+    const char* digits = "5";
 
-    const double result = convert_units(from, units_from, units_to);
+    Byte to = convert_units(digits, units_from, units_to);
+
+    /*const double result = convert_units(from, units_from, units_to);*/
+    const double result = mpfr_get_d(to.amt, MPFR_RNDF);
     const double expected = 5.242880;
     printf("Result  : %f\n", result);
     printf("Expected: %f\n", expected);
@@ -82,8 +86,13 @@ void convert_units_si_to_binary_should_return_correct_conversion() {
     const char* from = "5MB";
     const char* units_from = "MB";
     const char* units_to = "MiB";
+    const char* digits = "5";
 
-    const double result = convert_units(from, units_from, units_to);
+    Byte to = convert_units(digits, units_from, units_to);
+
+    /*const double result = convert_units(from, units_from, units_to);*/
+    const double result = mpfr_get_d(to.amt, MPFR_RNDF);
+
     const double expected = 4.768372;
     printf("Result  : %f\n", result);
     printf("Expected: %f\n", expected);
@@ -94,7 +103,12 @@ void convert_units_si_to_si_should_return_correct_conversion() {
     const char* from = "5GiB";
     const char* units_from = "GiB";
     const char* units_to = "MiB";
-    const double result = convert_units(from, units_from, units_to);
+    const char* digits = "5";
+
+    Byte to = convert_units(digits, units_from, units_to);
+
+    /*const double result = convert_units(from, units_from, units_to);*/
+    const double result = mpfr_get_d(to.amt, MPFR_RNDF);
     const double expected = 5120.00;
     TEST_ASSERT_EQUAL_FLOAT(expected, result);
 }
@@ -103,7 +117,11 @@ void convert_units_binary_to_binary_should_return_correct_conversion() {
     const char* from = "5GB";
     const char* units_from = "GB";
     const char* units_to = "MB";
-    const double result = convert_units(from, units_from, units_to);
+    const char* digits = "5";
+
+    Byte to = convert_units(digits, units_from, units_to);
+    const double result = mpfr_get_d(to.amt, MPFR_RNDF);
+
     const double expected = 5000.00;
     TEST_ASSERT_EQUAL_FLOAT(expected, result);
 }
@@ -115,7 +133,12 @@ void convert_units_byte_to_byte_should_return_correct_conversion() {
     const char* from = "5B";
     const char* units_from = "B";
     const char* units_to = "B";
-    const double result = convert_units(from, units_from, units_to);
+    /*const double result = convert_units(from, units_from, units_to);*/
+    const char* digits = "5";
+
+    Byte to = convert_units(digits, units_from, units_to);
+    const double result = mpfr_get_d(to.amt, MPFR_RNDF);
+
     const double expected = 5;
     TEST_ASSERT_EQUAL_FLOAT(expected, result);
 }
@@ -125,7 +148,12 @@ void convert_units_byte_to_si_should_return_correct_conversion() {
     const char* from = "5B";
     const char* units_from = "B";
     const char* units_to = "KB";
-    const double result = convert_units(from, units_from, units_to);
+    /*const double result = convert_units(from, units_from, units_to);*/
+    const char* digits = "5";
+
+    Byte to = convert_units(digits, units_from, units_to);
+    const double result = mpfr_get_d(to.amt, MPFR_RNDF);
+
     const double expected = 0.005;
     TEST_ASSERT_EQUAL_FLOAT(expected, result);
 }
@@ -135,7 +163,12 @@ void convert_units_byte_to_binary_should_return_correct_conversion() {
     const char* from = "5B";
     const char* units_from = "B";
     const char* units_to = "KiB";
-    const double result = convert_units(from, units_from, units_to);
+    /*const double result = convert_units(from, units_from, units_to);*/
+    const char* digits = "5";
+
+    Byte to = convert_units(digits, units_from, units_to);
+    const double result = mpfr_get_d(to.amt, MPFR_RNDF);
+
     const double expected = 0.00488281;
     TEST_ASSERT_EQUAL_FLOAT(expected, result);
 }
@@ -145,7 +178,12 @@ void convert_units_si_to_byte_should_return_correct_conversion() {
     const char* from = "5MB";
     const char* units_from = "MB";
     const char* units_to = "B";
-    const double result = convert_units(from, units_from, units_to);
+    /*const double result = convert_units(from, units_from, units_to);*/
+    const char* digits = "5";
+
+    Byte to = convert_units(digits, units_from, units_to);
+    const double result = mpfr_get_d(to.amt, MPFR_RNDF);
+
     const double expected = 5000000;
     TEST_ASSERT_EQUAL_FLOAT(expected, result);
 }
@@ -155,7 +193,12 @@ void convert_units_binary_to_byte_should_return_correct_conversion() {
     const char* from = "5MiB";
     const char* units_from = "MiB";
     const char* units_to = "B";
-    const double result = convert_units(from, units_from, units_to);
+    /*const double result = convert_units(from, units_from, units_to);*/
+    const char* digits = "5";
+
+    Byte to = convert_units(digits, units_from, units_to);
+    const double result = mpfr_get_d(to.amt, MPFR_RNDF);
+
     const double expected = 5242880;
     TEST_ASSERT_EQUAL_FLOAT(expected, result);
 }
@@ -165,7 +208,12 @@ void convert_units_petabyte_to_byte_should_return_correct_conversion() {
     const char* from = "5PB";
     const char* units_from = "PB";
     const char* units_to = "B";
-    const double result = convert_units(from, units_from, units_to);
+    /*const double result = convert_units(from, units_from, units_to);*/
+    const char* digits = "5";
+
+    Byte to = convert_units(digits, units_from, units_to);
+    const double result = mpfr_get_d(to.amt, MPFR_RNDF);
+
     const long double expected = 5000000000000000.00;
     TEST_ASSERT_EQUAL_FLOAT(expected, result);
 }
