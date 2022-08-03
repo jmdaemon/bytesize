ac7696a80fc7eaa6d76e091f97cd2c1681885d8d
Joseph Diza <josephm.diza@gmail.com>
2022-08-02 20:09:25 -0700

    Fix \'comparison of unsigned expression in ‘< 0’ is always false\'

--- src/cli.c
+++ src/cli.c
@@ -45,7 +45,7 @@ error_t parse_opt (int key, char *arg, struct argp_state *state) {
       break;
 
     case ARGP_KEY_END:
-      if (state->arg_num < 0) {
+      if (state->arg_num < 1) {
         /* Not enough arguments. */
         argp_usage (state);
       }
